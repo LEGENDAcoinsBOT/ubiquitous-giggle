30a427175ea18fa90000bbc51667914e769e2c58
Legenda <nikitavolk905@gmail.com>
2026-05-13 18:15:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI, Request
 import requests
-import os
 
-BOT_TOKEN = "8650861652:AAE0Q4-ONNBPj8Pv9YxAp6abJZN0Qhk1in0"
+BOT_TOKEN = "ТВОЙ_НОВЫЙ_ТОКЕН"
 
 app = FastAPI()
+
+
+def send_message(chat_id, text, keyboard=None):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    payload = {
+        "chat_id": chat_id,
+        "text": text,
+    }
+
+    if keyboard:
+        payload["reply_markup"] = keyboard
+
+    requests.post(url, json=payload)
 
 
 @app.get("/")
@@ -22,12 +35,40 @@ async def webhook(req: Request):
         chat_id = data["message"]["chat"]["id"]
         text = data["message"].get("text", "")
 
-        requests.post(
-            f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-            json={
-                "chat_id": chat_id,
-                "text": f"Вы написали: {text}"
-            }
-        )
+        keyboard = {
+            "inline_keyboard": [
+                [
+                    {"text": "💰 TikTok Coins", "callback_data": "coins"}
+                ],
+                [
+                    {"text": "⭐ Telegram Stars", "callback_data": "stars"}
+                ],
+                [
+                    {"text": "👑 Telegram Premium", "callback_data": "premium"}
+                ]
+            ]
+        }
+
+        if text == "/start":
+            send_message(
+                chat_id,
+                "Добро пожаловать в магазин 👋",
+                keyboard
+            )
+
+    if "callback_query" in data:
+        query = data["callback_query"]
+
+        chat_id = query["message"]["chat"]["id"]
+        data_btn = query["data"]
+
+        if data_btn == "coins":
+            send_message(chat_id, "Введите username TikTok")
+
+        elif data_btn == "stars":
+            send_message(chat_id, "Введите Telegram username")
+
+        elif data_btn == "premium":
+            send_message(chat_id, "Выберите срок Premium")
 
     return {"ok": True}
