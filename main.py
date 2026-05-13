82eb1ae7ee2bee9232834b51e7b3ae99e9dee7d4
Legenda <nikitavolk905@gmail.com>
2026-05-13 17:58:11 +0200

    Rename root function to home and add webhook

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def home():
     return {"status": "ok"}
+
+
+@app.post("/webhook")
+async def webhook(req: Request):
+    data = await req.json()
+    print(data)
+    return {"ok": True}
